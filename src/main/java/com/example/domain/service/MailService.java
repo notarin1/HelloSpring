88cf4a475df2cfbd9932f4c1895b72656d135ac5
Keisuke Inoue <notarin1jp@gmail.com>
2016-02-28 01:25:16 +0900

    implement-mail-sender
    メール送信機能を追加

--- src/main/java/com/example/domain/service/MailService.java
+++ src/main/java/com/example/domain/service/MailService.java
@@ -1,13 +1,27 @@
 package com.example.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import lombok.NonNull;
 
 @Service
 public class MailService {
-    public void sendEmail(@NonNull String email, @NonNull String body) {
-	// TODO メール送信をする
-	
+    @Autowired
+    private JavaMailSender sender;
+
+    public SimpleMailMessage sendEmail(@NonNull String email, @NonNull String subject, @NonNull String content) {
+	SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+	mailMessage.setTo(email);
+	mailMessage.setFrom("this_service@gmail.com");
+	mailMessage.setSubject(subject);
+	mailMessage.setText(content);
+
+	sender.send(mailMessage);
+
+	return mailMessage;
     }
 }
