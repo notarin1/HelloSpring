5f94cfb8c49281d86a6c31c31ad0c5f6408b4bf7
Keisuke Inoue <notarin1jp@gmail.com>
2016-02-27 23:52:47 +0900

    implement new account registration
    新規アカウント登録処理仮実装

---
+++ src/main/java/com/example/domain/service/MailService.java
@@ -0,0 +1,13 @@
+package com.example.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.NonNull;
+
+@Service
+public class MailService {
+    public void sendEmail(@NonNull String email, @NonNull String body) {
+	// TODO メール送信をする
+	
+    }
+}
